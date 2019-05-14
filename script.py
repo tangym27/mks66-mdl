102b18587ff3d5373cbf0bb9b4dc2cc5b0003d98
Michelle Tang <mtang1@stuy.edu>
2019-05-14 15:01:20 -0400

    finished

--- script.py
+++ script.py
@@ -46,70 +46,55 @@ def run(filename):
                           'blue': [0.2, 0.5, 0.5]}]
     reflect = '.white'
 
-    for line in commands:
-        print line
-        c = line['op']
-        args = line['args']
+    for command in commands:
+        line = command['op']
+        args = command['args']
+        if line == 'box':
+            if command['constants']:
+                reflect = command['constants']
+            else:
+                reflect = '.white'
 
-
-        if (not args == None):
-            args = line['args'][:]
-
-        if (not args ==  None) and 'knob' in line and (not line['knob'] == None) and (c == "move" or c == "scale" or c == "rotate"):
-            k = line['knob']
-            for i in range(len(args)):
-                if not isinstance(args[i], basestring):
-                    args[i] = args[i] * symbols[k][1]
-
-
-        if c == 'box':
-            if isinstance(args[0], str):
-                consts = args[0]
-                args = args[1:]
-            if isinstance(args[-1], str):
-                coords = args[-1]
             add_box(tmp,
                     args[0], args[1], args[2],
                     args[3], args[4], args[5])
             matrix_mult( stack[-1], tmp )
+            # print("\n\n")
+            # print reflect
             draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
             tmp = []
-        elif c == 'sphere':
+        elif line == 'sphere':
+            if command['constants']:
+                reflect = command['constants']
             add_sphere(tmp, args[0], args[1], args[2], args[3], step_3d)
             matrix_mult( stack[-1], tmp )
             draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
             tmp = []
-        elif c == 'torus':
+        elif line == 'torus':
+            if command['constants']:
+                reflect = command['constants']
             add_torus(tmp,
                   args[0], args[1], args[2], args[3], args[4], step_3d)
             matrix_mult( stack[-1], tmp )
             draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
             tmp = []
-        elif c == 'line':
-            if isinstance(args[0], str):
-                consts = args[0]
-                args = args[1:]
-            if isinstance(args[3], str):
-                coords = args[3]
-                args = args[:3] + args[4:]
-            if isinstance(args[-1], str):
-                coords1 = args[-1]
+        elif line == 'line':
             add_edge(tmp,
                  args[0], args[1], args[2], args[3], args[4], args[5])
             matrix_mult( stack[-1], tmp )
             draw_lines(tmp, screen, zbuffer, color)
             tmp = []
-        elif c == 'move':
+        elif line == 'move':
             tmp = make_translate(args[0], args[1], args[2])
             matrix_mult(stack[-1], tmp)
             stack[-1] = [x[:] for x in tmp]
             tmp = []
-        elif c == 'scale':
+        elif line == 'scale':
             tmp = make_scale(args[0], args[1], args[2])
             matrix_mult(stack[-1], tmp)
             stack[-1] = [x[:] for x in tmp]
             tmp = []
-        elif c == 'rotate':
+        elif line == 'rotate':
             theta = args[1] * (math.pi/180)
             if args[0] == 'x':
                 tmp = make_rotX(theta)
@@ -120,11 +105,11 @@ def run(filename):
             matrix_mult( stack[-1], tmp )
             stack[-1] = [ x[:] for x in tmp]
             tmp = []
-        elif c == 'push':
+        elif line == 'push':
             stack.append([x[:] for x in stack[-1]] )
-        elif c == 'pop':
+        elif line == 'pop':
             stack.pop()
-        elif c == 'display':
+        elif line == 'display':
             display(screen)
-        elif c == 'save':
+        elif line == 'save':
             save_extension(screen, args[0]+".png")
